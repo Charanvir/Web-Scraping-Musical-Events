f5b8bf83012f97274c985782152115d3f56b913f
Charanvir <charanvir123@gmail.com>
2023-01-24 14:19:45 -0500

    utilized selectorLib to get the desired text from the selected element

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import selectorlib
 
-
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
@@ -11,3 +10,14 @@ def scrape(url):
     source = response.text
     return source
 
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
